1217abbb179efff80b75e7c589780f3d05f50ba8
Audey Orcajada <audeyorcajada23@gmail.com>
2024-12-02 17:03:02 +0800

    5th commit

--- Settings.php
+++ Settings.php
@@ -13,6 +13,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Math Quiz Settings</title>
 </head>
 <body>
-    
+        <h1>Math Quiz Settings</h1>
+        <form method="post">
+            <label for="num_questions">Number of Questions:</label>
+            <input type="number" id="num_questions" name="num_questions" min="1" required><br><br>
+
+            <label for="quiz_type">Quiz Type:</label>
+            <select id="quiz_type" name="quiz_type" required>
+                <option value="addition">Addition</option>
+                <option value="subtraction">Subtraction</option>
+                <option value="multiplication">Multiplication</option>
+                <option value="division">Division</option>
+            </select><br><br>
+
+            <button type="submit">Start Quiz</button>
+        </form>
 </body>
 </html>
