4e9d9d5d28f76f203a258bfa7b8f391074d3ea29
Audey Orcajada <audeyorcajada23@gmail.com>
2024-12-02 17:34:38 +0800

    10th commit

--- Results.php
+++ Results.php
@@ -4,3 +4,7 @@ if (!isset($_SESSION['answers'], $_POST['response'])) {
     header('Location: Settings.php');
     exit;
 }
+
+$answers = $_SESSION['answers'];
+$responses = $_POST['response'];
+$score = 0;
