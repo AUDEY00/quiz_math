20732e03a52060bbc75106acce6ed112f46a9260
Audey Orcajada <audeyorcajada23@gmail.com>
2024-12-02 17:42:22 +0800

    11th commit

--- Results.php
+++ Results.php
@@ -8,3 +8,14 @@ if (!isset($_SESSION['answers'], $_POST['response'])) {
 $answers = $_SESSION['answers'];
 $responses = $_POST['response'];
 $score = 0;
+
+echo "<h1>Results</h1>";
+foreach ($responses as $index => $response) {
+    $correct = $answers[$index];
+    if ($response == $correct) {
+        echo "<p>Q" . ($index + 1) . ": Correct!</p>";
+        $score++;
+    } else {
+        echo "<p>Q" . ($index + 1) . ": Incorrect. Correct answer: $correct</p>";
+    }
+}
